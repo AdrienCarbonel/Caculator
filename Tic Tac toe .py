5bda83eda5dabdcea46afb181e89a2cc3d3536da
Adrien <adrien.carbonel@ens.uvsq.fr>
2022-12-22 13:48:38 +0100

    Construction Grille

--- Tic Tac toe .py
+++ Tic Tac toe .py
@@ -1,10 +1,15 @@
 from turtle import*
-from tkinter import*
 
 
+def carre():
+    for i in range(1,5):
+        forward(100)
+        left(90)
+        
 
 # Grille du Morpion
 for i in range(3):
     for j in range(3):
-        
+        carre()
+        forward(100)
 
