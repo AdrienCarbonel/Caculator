8dc37a99bb9595cb84d8e62750de5a9215440f47
Adrien <adrien.carbonel@ens.uvsq.fr>
2022-12-23 09:34:59 +0100

    Rien

--- Tic Tac toe .py
+++ Tic Tac toe .py
@@ -1,15 +1,2 @@
-from turtle import*
 
-
-def carre():
-    for i in range(1,5):
-        forward(100)
-        left(90)
-        
-
-# Grille du Morpion
-for i in range(3):
-    for j in range(3):
-        carre()
-        forward(100)
 
